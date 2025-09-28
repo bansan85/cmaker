f5213f449add0d0c4973f22c1185bfd962faec7b
Vincent Le Garrec <github@le-garrec.fr>
2025-09-28 21:57:41 +0200

    Initial commit

---
+++ CMaker/GlobalUsings.cs
@@ -0,0 +1,12 @@
+global using System.Collections.Immutable;
+global using Microsoft.Extensions.DependencyInjection;
+global using Microsoft.Extensions.Hosting;
+global using Microsoft.Extensions.Localization;
+global using Microsoft.Extensions.Logging;
+global using Microsoft.Extensions.Options;
+global using CMaker.Models;
+global using CMaker.Presentation;
+global using CMaker.Services.Endpoints;
+global using Uno.Extensions.Http.Kiota;
+global using ApplicationExecutionState = Windows.ApplicationModel.Activation.ApplicationExecutionState;
+[assembly: Uno.Extensions.Reactive.Config.BindableGenerationTool(3)]
---
+++ CMaker/Models/Entity.cs
@@ -0,0 +1,3 @@
+namespace CMaker.Models;
+
+public record Entity(string Name);
