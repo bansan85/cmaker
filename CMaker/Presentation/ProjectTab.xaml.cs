fe71507b22f67008b1367dfc834ffabe6038e4fe
Vincent Le Garrec <github@le-garrec.fr>
2025-09-30 22:54:44 +0200

    Split main xaml in multiple xaml

---
+++ CMaker/Presentation/ProjectTab.xaml.cs
@@ -0,0 +1,9 @@
+namespace CMaker.Presentation;
+
+public sealed partial class ProjectTab : UserControl
+{
+    public ProjectTab()
+    {
+        this.InitializeComponent();
+    }
+}
---
+++ CMaker/Presentation/TargetsTab.xaml.cs
@@ -0,0 +1,9 @@
+namespace CMaker.Presentation;
+
+public sealed partial class TargetsTab : UserControl
+{
+    public TargetsTab()
+    {
+        this.InitializeComponent();
+    }
+}
