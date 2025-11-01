9a5bef61bc0a8a61e2510c2e116d30b18aa9b8c1
Vincent Le Garrec <github@le-garrec.fr>
2025-11-01 20:32:27 +0100

    Iterate in all items

--- src/app/features/cmake-project/components/tab-project.ts
+++ src/app/features/cmake-project/components/tab-project.ts
@@ -1,4 +1,11 @@
-import { Component, Type } from "@angular/core";
+import {
+  AfterViewInit,
+  Component,
+  QueryList,
+  Type,
+  ViewChildren,
+  ViewContainerRef,
+} from "@angular/core";
 import { ProjectCommand } from "../../commands/components/project-command";
 import { CMakeMsvcRuntimeLibraryVariable } from "../../variables/components/cmake-msvc-runtime-library-variable";
 import { DraggableListComponent } from "../../../shared/components/list/draggable-list";
@@ -13,16 +20,43 @@ import { CMakeFeatureInterface } from "../../commands/services/cmake-feature-int
   templateUrl: "./tab-project.html",
   styleUrl: "./tab-project.css",
 })
-export class TabProject {
+export class TabProject implements AfterViewInit {
   items: Type<CMakeProvider<CMakeFeatureInterface<any>>>[] = [
     ProjectCommand,
     CMakeMsvcRuntimeLibraryVariable,
   ];
+
+  @ViewChildren("container", { read: ViewContainerRef })
+  containers!: QueryList<ViewContainerRef>;
+  container!: ViewContainerRef;
+  private instances: CMakeProvider<CMakeFeatureInterface<any>>[] = [];
+
+  ngAfterViewInit() {
+    this.instances = [];
+    const containers = this.containers.toArray();
+    containers.forEach((vcRef, i) => {
+      vcRef.clear();
+      const ref = vcRef.createComponent(this.items[i]);
+      this.instances.push(ref.instance);
+    });
+  }
 
   reorderItems(event: { from: number; to: number }) {
     [this.items[event.from], this.items[event.to]] = [
       this.items[event.to],
       this.items[event.from],
     ];
+    [this.instances[event.from], this.instances[event.to]] = [
+      this.instances[event.to],
+      this.instances[event.from],
+    ];
+  }
+
+  toto() {
+    console.log(this.instances);
+    this.instances.forEach((i) => {
+      console.log(i.service.cmakeRequiredVersion(i));
+      console.log(i.service.toCMakeListTxt(i));
+    });
   }
 }
