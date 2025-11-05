b1040aa465da11f2625d17070231be95a42c7458
Vincent Le Garrec <github@le-garrec.fr>
2025-11-05 23:42:58 +0100

    Add createComponent in ProjectCompatVersion tests

--- src/app/features/commands/services/project-compat-version-service.spec.ts
+++ src/app/features/commands/services/project-compat-version-service.spec.ts
@@ -1,15 +1,21 @@
-import { TestBed } from "@angular/core/testing";
+import { ComponentFixture, TestBed } from "@angular/core/testing";
 
 import { ProjectCompatVersionService } from "./project-compat-version-service";
+import { ProjectCompatVersionArgument } from "../components/project-compat-version-argument";
 
 describe("ProjectCompatVersionService", () => {
   let service: ProjectCompatVersionService;
+  let fixture: ComponentFixture<ProjectCompatVersionArgument>;
+  let component: ProjectCompatVersionArgument;
 
   beforeEach(() => {
     TestBed.configureTestingModule({
       providers: [ProjectCompatVersionService],
     });
     service = TestBed.inject(ProjectCompatVersionService);
+    fixture = TestBed.createComponent(ProjectCompatVersionArgument);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
   });
 
   it("should be created", () => {
