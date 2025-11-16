491c0c42345d973f8f8e453ef65dc3f85dec91e5
Vincent Le Garrec <github@le-garrec.fr>
2025-11-16 13:43:07 +0100

    Hide Checkboxes menu when click outside

--- src/app/shared/components/checkbox/checkboxes-list.ts
+++ src/app/shared/components/checkbox/checkboxes-list.ts
@@ -1,18 +1,29 @@
-import { Component, input } from "@angular/core";
-import { LucideAngularModule } from "lucide-angular";
+import { Component, ElementRef, inject, input } from '@angular/core'
+import { LucideAngularModule } from 'lucide-angular'
 
 @Component({
-  selector: "app-checkboxes-list",
+  selector: 'app-checkboxes-list',
   imports: [LucideAngularModule],
-  templateUrl: "./checkboxes-list.html",
-  styleUrl: "./checkboxes-list.css",
+  templateUrl: './checkboxes-list.html',
+  styleUrl: './checkboxes-list.css',
+  host: {
+    '(document:click)': 'onClickOutside($event)',
+  },
 })
 export class CheckboxesList {
-  expanded: boolean = false;
+  expanded: boolean = false
 
-  icon = input<string>("menu");
+  elementRef = inject(ElementRef)
+
+  icon = input<string>('menu')
 
   showCheckboxes() {
-    this.expanded = !this.expanded;
+    this.expanded = !this.expanded
+  }
+
+  onClickOutside(event: MouseEvent) {
+    if (!this.elementRef.nativeElement.contains(event.target)) {
+      this.expanded = false
+    }
   }
 }
