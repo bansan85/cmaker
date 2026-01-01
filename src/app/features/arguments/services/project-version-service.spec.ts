4975b1cfa3468383dc7b217089a5b0ae83cf98ae
Vincent Le Garrec <github@le-garrec.fr>
2026-01-01 18:37:21 +0100

    Improve tests for project VERSION service

--- src/app/features/arguments/services/project-version-service.spec.ts
+++ src/app/features/arguments/services/project-version-service.spec.ts
@@ -1,13 +1,15 @@
 import { TestBed } from '@angular/core/testing';
-import { beforeEach,describe, expect, it } from 'vitest';
+import { beforeEach, describe, expect, it } from 'vitest';
 
 import { DEFAULT_MAX_VERSION } from '../../../app.tokens';
+import { InputVersionModel } from '../../../shared/models/arguments/input-version-model';
 import { Version } from '../../../shared/models/version';
 import { ProjectContextService } from '../../cmake-project/services/project-context-service';
 import { ProjectVersionService } from './project-version-service';
 
 describe('ProjectVersionService', () => {
   let service: ProjectVersionService;
+  let projectContext: ProjectContextService;
 
   beforeEach(() => {
     TestBed.configureTestingModule({
@@ -21,9 +23,49 @@ describe('ProjectVersionService', () => {
       ],
     });
     service = TestBed.inject(ProjectVersionService);
+    projectContext = TestBed.inject(ProjectContextService);
   });
 
   it('should be created', () => {
     expect(service).toBeTruthy();
+  });
+
+  it('should support missing optional fields', async () => {
+    const action: InputVersionModel = {};
+    projectContext.maxCMakeVersion = undefined;
+
+    expect(service.isEnabled(action)).toBe(true);
+    expect(await service.isValid(action)).toBe(false);
+    expect(service.cmakeRequiredVersion(action)).toBeNull();
+    expect(service.cmakeObjects(action)).toBeTruthy();
+    expect(await service.toCMakeListTxt(action)).toBe(
+      '# Invalid\nVERSION undefined\n'
+    );
+    expect(service.toCMakerTxt(action)).toBe(`VERSION undefined\n`);
+    expect(service.isEffectiveVersionValid(action)).toBe(true);
+
+    action.enabled = false;
+    expect(service.cmakeRequiredVersion(action)).toBeNull();
+    expect(service.isEnabled(action)).toBe(false);
+    expect(service.cmakeObjects(action)).toBeNull();
+    expect(await service.toCMakeListTxt(action)).toBe('');
+    expect(service.toCMakerTxt(action)).toBe(`VERSION undefined\n`);
+
+    action.enabled = true;
+    expect(service.isEnabled(action)).toBe(true);
+
+    projectContext.maxCMakeVersion = new Version(3, 0);
+    expect(service.isEnabled(action)).toBe(true);
+
+    projectContext.maxCMakeVersion = new Version(4, 3);
+    expect(service.isEnabled(action)).toBe(true);
+
+    action.version = new Version(1, 2, 3, 4);
+    expect(service.isEnabled(action)).toBe(true);
+    expect(await service.isValid(action)).toBe(true);
+    expect(service.cmakeObjects(action)).toBeTruthy();
+    expect(await service.toCMakeListTxt(action)).toBe('VERSION 1.2.3.4\n');
+    expect(service.toCMakerTxt(action)).toBe(`VERSION 1.2.3.4\n`);
+    expect(service.isEffectiveVersionValid(action)).toBe(true);
   });
 });
