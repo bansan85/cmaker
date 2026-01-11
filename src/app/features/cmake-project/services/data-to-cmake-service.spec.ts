adc8f1fac8c8fb6077a006892adb89ab750991ae
Vincent Le Garrec <github@le-garrec.fr>
2026-01-11 19:55:16 +0100

    Add tests for DataToCMakeService

--- src/app/features/cmake-project/services/data-to-cmake-service.spec.ts
+++ src/app/features/cmake-project/services/data-to-cmake-service.spec.ts
@@ -1,6 +1,7 @@
 import { TestBed } from '@angular/core/testing';
 import { beforeEach, describe, expect, it } from 'vitest';
 
+import { Version } from '../../../shared/models/version';
 import { DataToCMakeService } from './data-to-cmake-service';
 
 describe('DataToCMakeService', () => {
@@ -16,5 +17,20 @@ describe('DataToCMakeService', () => {
 
     expect(service.booleanToString(true)).toEqual('ON');
     expect(service.booleanToString(false)).toEqual('OFF');
+
+    expect(service.stringToBoolean('ON')).toEqual(true);
+    expect(service.stringToBoolean('OFF')).toEqual(false);
+
+    expect(service.stringToVersion('trez')).toBeUndefined();
+    expect(service.stringToVersion('1.2.4')).toStrictEqual(
+      new Version(1, 2, 4)
+    );
+
+    expect(service.isValidTargetName('ON')).toEqual(true);
+    expect(service.isValidTargetName('"\'(-è_iokjn)\'"')).toEqual(false);
+
+    expect(
+      service.stringToCMakeName('A<PROJECT-NAME>B', { project: 'TOTO' })
+    ).toEqual('ATOTOB');
   });
 });
