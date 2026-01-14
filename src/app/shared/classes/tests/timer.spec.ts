273d6daf1c58b2bdab53175181ca75328f0ec230
Vincent Le Garrec <github@le-garrec.fr>
2026-01-14 10:50:25 +0100

    Fix timer test

--- src/app/shared/classes/tests/timer.spec.ts
+++ src/app/shared/classes/tests/timer.spec.ts
@@ -4,22 +4,27 @@ import { sleep } from './timer';
 
 describe('MockIpc', () => {
   it('should wait', async () => {
+    const targetError = 1;
+
+    let timerTarget = 10;
     let date1 = new Date();
-    await sleep(10);
+    await sleep(timerTarget);
     let date2 = new Date();
     let diff = date2.getTime() - date1.getTime();
-    expect(diff).greaterThanOrEqual(10);
+    expect(diff).greaterThanOrEqual(timerTarget - targetError);
 
+    timerTarget = 100;
     date1 = new Date();
-    await sleep(100);
+    await sleep(timerTarget);
     date2 = new Date();
     diff = date2.getTime() - date1.getTime();
-    expect(diff).greaterThanOrEqual(100);
+    expect(diff).greaterThanOrEqual(timerTarget - targetError);
 
+    timerTarget = 150;
     date1 = new Date();
-    await sleep(150);
+    await sleep(timerTarget);
     date2 = new Date();
     diff = date2.getTime() - date1.getTime();
-    expect(diff).greaterThanOrEqual(150);
+    expect(diff).greaterThanOrEqual(timerTarget - targetError);
   });
 });
